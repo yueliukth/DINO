b05196d6a1ad9832fd86c1014c18ecf1580901be
Yue Liu <yue3@kth.se>
2021-11-01 21:26:40 +0100

    Added codes to load an example Dataset with distributed training
    For debugging purpose

--- scripts/helper.py
+++ scripts/helper.py
@@ -1,4 +1,69 @@
+import os
 import sys
+import numpy as np
+import torch
+import torch.distributed as dist
+import torch.multiprocessing as mp
+
+def _find_free_port():
+    import socket
+
+    sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    # Binding to port 0 will cause the OS to find an available port for us
+    sock.bind(("", 0))
+    port = sock.getsockname()[1]
+    sock.close()
+    # NOTE: there is still a chance the port could be taken by other processes.
+    return port
+
+def ddp_is_on():
+    if not dist.is_available():
+        return False
+    if not dist.is_initialized():
+        return False
+    world_size = dist.get_world_size()
+    if world_size < 2:
+        return False
+    return True
+
+def synchronize():
+    if not ddp_is_on():
+        return
+    dist.barrier()
+
+def set_sys_params(params):
+    # Set gpu params
+    os.environ["CUDA_VISIBLE_DEVICES"] = params['gpu_ids']
+    # Set random seeds
+    torch.manual_seed(params['random_seed'])
+    torch.cuda.manual_seed_all(params['random_seed'])
+    np.random.seed(params['random_seed'])
+
+def _distributed_worker(rank, main_func, world_size, dist_url, args):
+    dist.init_process_group(backend="NCCL", init_method=dist_url, world_size=world_size, rank=rank)
+
+    # synchronize is needed here to prevent a possible timeout after calling init_process_group
+    # See: https://github.com/facebookresearch/maskrcnn-benchmark/issues/172
+    synchronize()
+
+    torch.cuda.set_device(rank)
+    main_func(args)
+
+def launch(main_func, args=()):
+    system_params = args['system_params']
+    set_sys_params(system_params)
+
+    world_size = system_params['num_gpus']
+    port = _find_free_port()
+    dist_url = f"tcp://127.0.0.1:{port}"
+
+    mp.spawn(
+        _distributed_worker,
+        nprocs=world_size,
+        args=(main_func, world_size, dist_url, args),
+        daemon=False,
+    )
+
 
 def print_seperate_line():
     print()
--- scripts/main.py
+++ scripts/main.py
@@ -1,21 +1,67 @@
+import os
 import sys
 import yaml
+import argparse
 
+import numpy as np
+
+import torch
+from torch.utils.data import Dataset
+from torchvision import datasets
+from torchvision.transforms import ToTensor
+from torch.utils.data import DataLoader
 import helper
 
-def read_params(file):
-    with open(file) as f:
-        args = yaml.safe_load(f)
+def parse_args(params_path=None):
+    parser = argparse.ArgumentParser(description='DINO reimplementation')
 
-    helper.print_seperate_line()
+    if params_path == None:
+        parser.add_argument('--params_path', type=str, required=False, help='Give a valid yaml file that contains all params to load.')
+        params_path = parser.parse_args().params_path
+
+    with open(params_path) as f:
+        args = yaml.safe_load(f)
+    
+    print(end='\n\n'+'=='*50+'\n\n')
     print('ARGS ARE: ')
-    print(args)
-    helper.print_seperate_line()
+    print(args, end='\n\n'+'=='*50+'\n\n')
     return args
 
 def train_process(args, start_training=True):
+    system_params = args['system_params']
+    dataloader_params = args['dataloader_params']
+
     print('Start preparing for training ')
     # TODO: Set up data loader with augmentations
+    # Load an example Dataset with distributed training, for debugging purpose
+    training_data = datasets.FashionMNIST(
+        root="../data",
+        train=True,
+        download=True,
+        transform=ToTensor()
+    )
+
+    test_data = datasets.FashionMNIST(
+        root="../data",
+        train=False,
+        download=True,
+        transform=ToTensor()
+    )
+
+    # Set sampler that restricts data loading to a subset of the dataset
+    # In conjunction with torch.nn.parallel.DistributedDataParallel
+    training_sampler = torch.utils.data.DistributedSampler(training_data, shuffle=True)
+    test_sampler = torch.utils.data.DistributedSampler(test_data, shuffle=True)
+
+    # Preparing the data for training with DataLoaders
+    trainloader_params = dataloader_params['trainloader']
+    valloader_params = dataloader_params['valloader']
+    train_dataloader = DataLoader(training_data, batch_size=int(trainloader_params['batch_size']/system_params['num_gpus']),
+                                  num_workers=trainloader_params['num_workers'], pin_memory=trainloader_params['pin_memory'], drop_last=trainloader_params['drop_last'])
+    test_dataloader = DataLoader(test_data, batch_size=int(valloader_params['batch_size']/system_params['num_gpus']),
+                                 num_workers=valloader_params['num_workers'], pin_memory=valloader_params['pin_memory'], drop_last=valloader_params['drop_last'])
+    print(f"Data loaded: there are {len(training_data)} training images.")
+    print(f"Data loaded: there are {len(test_data)} test images.")
 
     # TODO: Build the student and teacher networks
 
@@ -23,13 +69,12 @@ def train_process(args, start_training=True):
 
     return
 
-def main(params_file):
-    # Read params and print them
-    args = read_params(file=params_file)
-
+def main(args):
     # Set up training
     train_process(args, start_training=True)
 
-
 if __name__ == '__main__':
-    main(params_file='yaml/test_params.yaml')
+    # Read params and print them
+    args = parse_args(params_path='yaml/test_params.yaml')
+    # Launch multi-gpu or distributed training
+    helper.launch(main, args)
