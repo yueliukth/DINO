0439081b39e672eaad2ef9c479bb44c583b6c20d
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-29 12:24:54 +0000

    Adding tarfile member sanitization to extractall()

--- scripts/prepare_datasets.py
+++ scripts/prepare_datasets.py
@@ -165,7 +165,26 @@ class GetDatasets():
                     download_url(dataset_url, target_path)
                     # Extract from archive
                     with tarfile.open(os.path.join(target_path, 'oxford-102-flowers.tgz'), 'r:gz') as tar:
-                        tar.extractall(path=target_path)
+                        def is_within_directory(directory, target):
+                            
+                            abs_directory = os.path.abspath(directory)
+                            abs_target = os.path.abspath(target)
+                        
+                            prefix = os.path.commonprefix([abs_directory, abs_target])
+                            
+                            return prefix == abs_directory
+                        
+                        def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+                        
+                            for member in tar.getmembers():
+                                member_path = os.path.join(path, member.name)
+                                if not is_within_directory(path, member_path):
+                                    raise Exception("Attempted Path Traversal in Tar File")
+                        
+                            tar.extractall(path, members, numeric_owner=numeric_owner) 
+                            
+                        
+                        safe_extract(tar, path=target_path)
 
                 img_folder = os.path.join(target_path, 'oxford-102-flowers')
                 if 'train' in official_split:
